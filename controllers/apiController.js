a02511252f59345d040097c2440fd234c3875b2c
rfaaelnc <rfaaelnc@gmail.com>
2021-08-15 02:50:48 -0300

    commit teste

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,3 +1,13 @@
 function teste() {
   alert(123);
+}function teste() {
+  alert(123);
+}function teste() {
+  alert(123);
+}function teste() {
+  alert(123);
+}function teste() {
+  alert(123);
+}function teste() {
+  alert(123);
 }
