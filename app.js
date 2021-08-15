a2b8e06292d29223e751b6b5f575513ee212c398
rfaaelnc <rfaaelnc@gmail.com>
2021-08-15 14:07:44 -0300

    Establish the connection with the B.D

--- app.js
+++ app.js
@@ -18,6 +18,32 @@ const bodyParser = require('body-parser');
 // this middleware must be aboce the routes handlers 
 app.use(bodyParser.json());
 
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://robot:0yVmhc4oztJY0kvO@nodejscluster.9ugyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  
+  console.log(err);
+
+  if(err) {
+    console.log('Error in connection', err);
+  }
+
+  // console.log(client);
+
+  client.close();
+  // app.listen(3001, function() {
+  //   console.log('server connected in port 3001');
+  // });
+
+});
+
+client.on('connectionReady', ()=> {
+  console.log('Server connected');
+});
+
 
 // END POINT INVÁLIDO
 app.get('/', function(req, res) {
